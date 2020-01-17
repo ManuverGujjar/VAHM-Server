86e689d27c2b171b6781f3fe9d26f81a796f6da6
Manuver Gujjar <manuver@Manuvers-MacBook-Air.local>
2020-01-17 15:25:21 +0530

    ase hi

--- src/app/App.java
+++ src/app/App.java
@@ -2,9 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /** Create a New Server Instance */
-        Server server = new Server();
-        /** Put server in listen state for listening for new connections */
-        server.acceptConnections();
+        
     }
 }
