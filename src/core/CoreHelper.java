33dba4ab72ad374934a49b85844d73ad2b258ede
Manuver Gujjar <ManvverGujjar@gmail.com>
2020-02-16 23:27:04 +0530

    added multithreading support

--- src/core/CoreHelper.java
+++ src/core/CoreHelper.java
@@ -1,8 +1,11 @@
 package core;
 
+import java.io.InputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+
+import core.Server.URLs;
 
 class CoreHelper {
     private List<HelperThread> helperThreads;
@@ -20,13 +23,28 @@ class CoreHelper {
     private class HelperThread extends Thread {
 
         private Socket socket;
+        private Request request;
+        private Response response;
+
         HelperThread(Socket socket) {
             this.socket = socket;
+            
         }
         
         @Override
         public void start() {
-            
+            try {
+                
+                request = new Request(socket.getInputStream());
+                response = new Response(socket.getOutputStream());
+
+                URLs.handleMaping(request.path.strip(), request, response);
+                
+                // socket.getOutputStream().write(new String("HTTP/1.1 200 OK\r\nContent-Length: 5\r\nConnection: close\r\n\nHello").getBytes());
+            } catch(Exception e) {
+                response.setStatus(404).send("url not found on server");
+                e.printStackTrace();
+            }
         }
     }
 }
