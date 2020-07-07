3cdada1bc8938517fd7394355a56548a382ebd89
Manuver Gujjar <ManvverGujjar@gmail.com>
2020-07-07 09:53:54 +0530

    added favicon.ico example

--- src/main/java/com/manuver/app/App.java
+++ src/main/java/com/manuver/app/App.java
@@ -1,25 +1,35 @@
 package com.manuver.app;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import com.manuver.core.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Server server = new Server();
 
-        server.get("/", (Request req, Response res) -> {
+        server.get("/get", (Request req, Response res) -> {
             res.setContentType("text/html").setStatus(200).send("Hello World");
         });
 
-        server.get("/hello", (Request req, Response res) -> {
-            res.setContentType("text/html").setStatus(200).send("this is url hello");
+        server.get("/", (Request req, Response res) -> {
+            res.setContentType("text/html").setStatus(400).send("not found");
+        });
+
+        server.get("/favicon.ico", (Request req, Response res) -> {
+            try {
+                InputStream in = new FileInputStream(new File("/Users/manuver/Downloads/pin.ico"));
+                res.setContentType("image/x-icon").setStatus(200).send(in.readAllBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
+                res.setStatus(404).send("not present on server");
+            }
         });
 
 
-        server.get("/hi", (Request req, Response res) -> {
-            res.setContentType("text/html").setStatus(200).send("this is url hi");
-        });
-
-        server.listen(1000, (Exception e) -> {
+        server.listen(2000, (Exception e) -> {
             e.printStackTrace();
         });
     }
