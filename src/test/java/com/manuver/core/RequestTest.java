4267ab5494d280c0ec1f126e67ea0899216afff9
Manuver Gujjar <ManvverGujjar@gmail.com>
2020-07-07 06:30:22 +0530

    added basic test for request class

---
+++ src/test/java/com/manuver/core/RequestTest.java
@@ -0,0 +1,37 @@
+package com.manuver.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class RequestTest {
+    
+    @Test
+    void testRequestUrl() {
+        String req = "GET /url?name=manuver HTTP/1.1\n\r";
+        InputStream inputStream = new ByteArrayInputStream(req.getBytes());
+        Request request = new Request(inputStream);
+        assertEquals("GET", request.method);
+        assertEquals("/url", request.path);
+    }  
+
+    @Test 
+    void testQueryString() {
+        String req = "GET /url?name=manuver HTTP/1.1\n\r";
+        InputStream inputStream = new ByteArrayInputStream(req.getBytes());
+        Request request = new Request(inputStream);
+        HashMap<String, String> expected = new HashMap<>();
+        expected.put("name", "manuver");
+        assertEquals(expected, request.query);
+        for (String key : expected.keySet()){
+            assertEquals(expected.get(key), request.query.get(key));
+        }
+    }
+
+}
